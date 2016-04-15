96afec3d1647abecfaac570c991367e95a4ded47
DAB0mB <emanor6@gmail.com>
2016-04-15 13:52:04 +0300

    Bootstrap application and resort load order

--- www/js/app.js
+++ www/js/app.js
@@ -1,7 +1,23 @@
 angular
   .module('whatsapp', [
-    'ionic',
     'angular-meteor',
     'angular-meteor.auth',
-    'angularMoment'
+    'angularMoment',
+    'ionic'
   ]);
+
+ionic.Platform.ready(function() {
+  angular.bootstrap(document, ['whatsapp']);
+
+  // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+  // for form inputs)
+  if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    cordova.plugins.Keyboard.disableScroll(true);
+
+  }
+  if (window.StatusBar) {
+    // org.apache.cordova.statusbar required
+    StatusBar.styleLightContent();
+  }
+});
